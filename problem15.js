55386de9ce6ef325da91c47c186e607a3f5b663a
SrRecursive <rojohnibana@gmail.com>
2023-12-03 16:40:14 +0100

    Problem15 solved

---
+++ problem15.js
@@ -0,0 +1,15 @@
+function latticePaths(gridSize)
+{
+	let result = 1;
+	let n = gridSize * 2
+	let k = gridSize
+
+	for (let i = 1; i < Math.min(k, n - k) + 1; i++)
+		result *= (n + 1 - i) / i;
+	return result;
+}
+
+console.log(latticePaths(2));
+console.log(latticePaths(3));
+console.log(latticePaths(4));
+console.log(latticePaths(20));
